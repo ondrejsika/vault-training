f63cf947df928d2951fadf059f1e56f87d69b6fc
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-06 17:14:59 +0200

    feat(examples/terraform): Add Vault configuration example using Terraform

---
+++ examples/terraform/main.tf
@@ -0,0 +1,118 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.4.1"
+    }
+  }
+}
+
+provider "vault" {
+  address = "https://vault.k8s.sikademo.com"
+}
+
+resource "vault_auth_backend" "userpass" {
+  type = "userpass"
+}
+
+resource "vault_policy" "super-admin" {
+  name = "super-admin"
+
+  policy = <<EOT
+path "*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
+resource "vault_policy" "read-all-secret" {
+  name = "read-all-secret"
+
+  policy = <<EOT
+path "secret/metadata/*" {
+  capabilities = ["list", "read"]
+}
+path "secret/data/*" {
+  capabilities = ["read", "list"]
+}
+EOT
+}
+
+resource "vault_policy" "s3" {
+  name = "s3"
+
+  policy = <<EOT
+path "aws/creds/s3" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+resource "vault_mount" "kv2-secret" {
+  path = "secret"
+  type = "kv-v2"
+}
+
+resource "vault_mount" "aws" {
+  path = "aws"
+  type = "aws"
+}
+
+resource "vault_aws_secret_backend_role" "s3" {
+  backend         = "aws"
+  name            = "s3"
+  credential_type = "iam_user"
+
+  policy_document = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+resource "vault_identity_entity" "admin" {
+  name = "admin-user"
+  policies = [
+    vault_policy.super-admin.name,
+  ]
+}
+
+resource "vault_identity_entity_alias" "admin" {
+  name           = vault_identity_entity.admin.name
+  mount_accessor = vault_auth_backend.userpass.accessor
+  canonical_id   = vault_identity_entity.admin.id
+}
+
+
+resource "vault_identity_entity" "ondrej" {
+  name = "ondrej-entity"
+  policies = [
+    vault_policy.read-all-secret.name,
+  ]
+}
+
+resource "vault_identity_entity_alias" "ondrej" {
+  name           = "ondrej"
+  mount_accessor = vault_auth_backend.userpass.accessor
+  canonical_id   = vault_identity_entity.ondrej.id
+}
+
+resource "vault_identity_entity" "s3" {
+  name = "s3-entity"
+  policies = [
+    vault_policy.s3.name,
+  ]
+}
+
+resource "vault_identity_entity_alias" "s3" {
+  name           = "s3"
+  mount_accessor = vault_auth_backend.userpass.accessor
+  canonical_id   = vault_identity_entity.s3.id
+}
