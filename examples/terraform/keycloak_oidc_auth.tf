10e0eaaf2b5071f1f2f876fac5c526cad3a86b83
Ondrej Sika <ondrej@ondrejsika.com>
2023-09-14 08:08:24 +0200

    feat(examples/terraform): Add admin role to Keycloak OIDC example

--- examples/terraform/keycloak_oidc_auth.tf
+++ examples/terraform/keycloak_oidc_auth.tf
@@ -1,9 +1,21 @@
+locals {
+  allowed_redirect_uris = [
+    "http://127.0.0.1:8250/oidc/callback",
+    "http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback",
+
+    "http://localhost:8250/oidc/callback",
+    "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
+
+    "https://vault.k8s.sikademo.com/ui/vault/auth/oidc/oidc/callback",
+  ]
+}
+
 resource "vault_jwt_auth_backend" "keycloak_oidc" {
   description        = "Keycloak OIDC authentication"
   path               = "oidc"
-  oidc_discovery_url = "https://sso.sikademo.com/realms/example"
-  oidc_client_id     = "example"
-  oidc_client_secret = "example"
+  oidc_discovery_url = "https://sso.sikademo.com/realms/sikademo"
+  oidc_client_id     = "vault"
+  oidc_client_secret = "vault"
   default_role       = "default"
 }
 
@@ -14,18 +26,31 @@ resource "vault_jwt_auth_backend_role" "default" {
   bound_audiences = [
     vault_jwt_auth_backend.keycloak_oidc.oidc_client_id,
   ]
-  allowed_redirect_uris = [
-    "http://localhost:8250/oidc/callback",
-    "http://localhost:8200/ui/vault/auth/oidc/oidc/callback",
-    "https://vault.k8s.sikademo.com/ui/vault/auth/oidc/oidc/callback",
-  ]
+  allowed_redirect_uris = local.allowed_redirect_uris
   token_policies = [
     vault_policy.read-all-secret.name,
   ]
 }
 
+resource "vault_jwt_auth_backend_role" "admin" {
+  backend      = vault_jwt_auth_backend.keycloak_oidc.path
+  role_name    = "admin"
+  user_claim   = "sub"
+  groups_claim = "groups"
+  bound_claims = {
+    "groups" = "vault-admin"
+  }
+  bound_audiences = [
+    vault_jwt_auth_backend.keycloak_oidc.oidc_client_id,
+  ]
+  allowed_redirect_uris = local.allowed_redirect_uris
+  token_policies = [
+    vault_policy.super-admin.name,
+  ]
+}
+
 resource "vault_identity_group" "admin" {
-  name = "admin"
+  name = "vault-admin"
   type = "external"
   policies = [
     vault_policy.super-admin.name,
@@ -33,7 +58,7 @@ resource "vault_identity_group" "admin" {
 }
 
 resource "vault_identity_group" "read-all-secret" {
-  name = "reader"
+  name = "vault-reader"
   type = "external"
   policies = [
     vault_policy.read-all-secret.name,
